dc83c9553c2fad78649eac1e9126b0ca16601339
shamalgithub <shamalgithub@gmail.com>
2024-07-06 12:48:42 +0530

    robbing house recursion is correct logic is wrong for n-1 instance

---
+++ recursion/robbing_houses_198.py
@@ -0,0 +1,18 @@
+from typing import List 
+
+# the recursion is correct the logic is wrong for n-1 instance can correct for n 
+def findmax(array:List , i:int)->int:
+    j = len(array)-1 - i
+    print("j" , j , "i" , i)
+    
+    if j == 1:
+        return array[j-1]
+    elif j == 0:
+        return array[j]
+    else:
+        right = array[j]
+        i += 2
+        return findmax(array=array , i=i) + right 
+
+# print((findmax(array=[2,7,9,3,1], i=0) , findmax(array=[2,7,9,3,1] , i=1)))
+print(findmax(array=[2 , 7, 9, 3,1] , i =1))
